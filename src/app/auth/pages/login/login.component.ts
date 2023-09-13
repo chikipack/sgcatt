1500f6be53c8d1d287b2a3c70decd11bf31ed03b
chikipack <victorgotzilla@hotmail.com>
2023-09-13 13:35:00 -0700

    auth componenets +login +register +forgot pass

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -12,9 +12,8 @@ export class LoginComponent {
   user: any;
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
-  ) // private authService: AuthService
-  {
+    private router: Router // private authService: AuthService
+  ) {
     this.loginForm = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
