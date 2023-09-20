539098d461a2535460e2c0270e87d4365e0d4ae1
chikipack <victorgotzilla@hotmail.com>
2023-09-20 12:02:22 -0700

    formulario de login -ahora usa matricula en lugar de correo

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-// import { AuthService } from '../../auth.service';
+import { AuthService } from '../../auth.service';
+import { CookieService } from 'ngx-cookie-service';
+
+interface TokenResponse {
+  access_token: string;
+  token_type: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +19,9 @@ export class LoginComponent {
   user: any;
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router // private authService: AuthService
+    private router: Router,
+    private authService: AuthService,
+    private cookieService: CookieService
   ) {
     this.loginForm = this.formBuilder.group({
       email: ['', Validators.required],
@@ -21,9 +30,17 @@ export class LoginComponent {
   }
 
   onSubmit() {
-    // this.authService.
-    this.router.navigate(['home'], { skipLocationChange: false }).then(() => {
-      window.location.reload();
-    });
+    console.log(this.loginForm);
+    this.authService
+      .getAuthToken(this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe((res: TokenResponse) => {
+        console.log(res);
+        this.cookieService.set('token', res.access_token, 1);
+        this.router
+          .navigate(['student/home'], { skipLocationChange: false })
+          .then(() => {
+            window.location.reload();
+          });
+      });
   }
 }
