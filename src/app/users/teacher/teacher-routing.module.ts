cb76adc1127119839dd5b7426d6e6bacfe838aa3
chikipack <victorgotzilla@hotmail.com>
2023-09-20 12:01:01 -0700

    teacher module y routing

--- src/app/users/teacher/teacher-routing.module.ts
+++ src/app/users/teacher/teacher-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeacherHomeComponent } from './teacher-home/teacher-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'home', component: TeacherHomeComponent },
+      { path: '**', redirectTo: 'home' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TeacherRoutingModule { }
+export class TeacherRoutingModule {}
--- src/app/users/teacher/teacher.module.ts
+++ src/app/users/teacher/teacher.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TeacherRoutingModule } from './teacher-routing.module';
-
+import { TeacherHomeComponent } from './teacher-home/teacher-home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    TeacherRoutingModule
-  ]
+  declarations: [TeacherHomeComponent],
+  imports: [CommonModule, TeacherRoutingModule],
 })
-export class TeacherModule { }
+export class TeacherModule {}
