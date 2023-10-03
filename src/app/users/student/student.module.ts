e0de36c8352f8078f5c6596f9083e8db069c24ae
chikipack <victorgotzilla@hotmail.com>
2023-10-03 16:51:49 -0700

    addded the imports to home component works

--- src/app/users/student/student.module.ts
+++ src/app/users/student/student.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { StudentRoutingModule } from './student-routing.module';
 
 import { HomeComponent } from './home/home.component';
+import { MyttComponent } from './mytt/mytt.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, StudentRoutingModule],
+  declarations: [HomeComponent, MyttComponent],
+  imports: [
+    CommonModule,
+    StudentRoutingModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class StudentModule {}
