d8d0282441b7d6750e72b7cd724a71ad51b68b8b
chikipack <victorgotzilla@hotmail.com>
2023-10-03 17:34:50 -0700

    teacher UI 1st addings

--- src/app/users/teacher/teacher.module.ts
+++ src/app/users/teacher/teacher.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 import { TeacherRoutingModule } from './teacher-routing.module';
 import { TeacherHomeComponent } from './teacher-home/teacher-home.component';
 
 @NgModule({
   declarations: [TeacherHomeComponent],
-  imports: [CommonModule, TeacherRoutingModule],
+  imports: [
+    CommonModule,
+    TeacherRoutingModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class TeacherModule {}
