0ec93832a126a5b47cfa6bd125057d771a12d1fe
chikipack <victorgotzilla@hotmail.com>
2023-10-03 17:58:41 -0700

    admin modules

---
+++ src/app/users/admin/admin-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'home', component: AdminHomeComponent },
+      { path: '**', redirectTo: 'home' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AdminRoutingModule {}
