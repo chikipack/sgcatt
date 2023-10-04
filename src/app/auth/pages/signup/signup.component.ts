ef4de54a5304de046f97c982f3f427b06b9e7ece
chikipack <victorgotzilla@hotmail.com>
2023-10-04 14:00:19 -0700

    sign up form

--- src/app/auth/pages/signup/signup.component.ts
+++ src/app/auth/pages/signup/signup.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  signupForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    // private router: Router,
+    private authService: AuthService,
+    private cookieService: CookieService
+  ) {
+    this.signupForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      middleName: ['', Validators.required],
+      institutionalEmail: ['', [Validators.required, Validators.email]],
+      personalEmail: ['', [Validators.required, Validators.email]],
+      matriculationNumber: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
+  onSubmit() {}
 }
