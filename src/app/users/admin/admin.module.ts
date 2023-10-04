35224cc3af2305d4d92b8f97222c1c4f006da4cb
chikipack <victorgotzilla@hotmail.com>
2023-10-03 18:19:50 -0700

    UI to admin

--- src/app/users/admin/admin.module.ts
+++ src/app/users/admin/admin.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [],
+  declarations: [AdminHomeComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
-export class AdminModule { }
+export class AdminModule {}
