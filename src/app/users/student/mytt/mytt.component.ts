c847a2da77014e0e0cebb31ca4471c1776e517cb
chikipack <victorgotzilla@hotmail.com>
2023-10-10 09:51:37 -0700

    dummy data added to the view of projects

--- src/app/users/student/mytt/mytt.component.ts
+++ src/app/users/student/mytt/mytt.component.ts
@@ -6,6 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mytt.component.css'],
 })
 export class MyttComponent implements OnInit {
+  data = [
+    {
+      title: 'Project A',
+      date: '2023-10-09',
+      students: ['Lorem', 'Ipsum', 'Dolor'],
+      teachers: ['Sit', 'Amet', 'Consectetur'],
+      panelists: ['Adipiscing', 'Elit', 'Sed'],
+      status: 'Failed',
+    },
+    {
+      title: 'Project B',
+      date: '2023-10-10',
+      students: ['Lorem', 'Ipsum', 'Dolor', 'Sit'],
+      teachers: ['Amet', 'Consectetur', 'Adipiscing'],
+      panelists: ['Elit', 'Sed', 'Do', 'Eiusmod'],
+      status: 'Failed',
+    },
+    {
+      title: 'Project C',
+      date: '2023-10-11',
+      students: ['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet'],
+      teachers: ['Consectetur', 'Adipiscing', 'Elit', 'Sed'],
+      panelists: ['Do', 'Eiusmod', 'Tempor', 'Incididunt'],
+      status: 'In progress',
+    },
+  ];
+
   hasProjects!: boolean;
   constructor() {}
 
