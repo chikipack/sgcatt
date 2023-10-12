1383113cf83f3ea41d43c7c5db506d72de74e125
chikipack <victorgotzilla@hotmail.com>
2023-10-12 15:55:58 -0700

    interfaz dummy para las invitaciones

--- src/app/users/student/invitations/invitations.component.ts
+++ src/app/users/student/invitations/invitations.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-invitations',
   templateUrl: './invitations.component.html',
-  styleUrls: ['./invitations.component.css']
+  styleUrls: ['./invitations.component.css'],
 })
 export class InvitationsComponent {
-
+  hasInvitations: boolean = false;
+  changeView() {
+    this.hasInvitations = !this.hasInvitations;
+  }
 }
